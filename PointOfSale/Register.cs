4dae49e43ad61332c19adc07ea4d165283729d2f
rchevuru <rchevuru@CIS-RDS2>
2020-10-21 16:35:29 -0500

    Finish

--- PointOfSale/Register.cs
+++ PointOfSale/Register.cs
@@ -11,5 +11,41 @@ namespace PointOfSale
         {
             return CardReader.RunCard(total);
         }
+
+        public int Pennies
+        {
+            get => CashDrawer.Pennies; 
+        }
+
+        public int Nickels
+        {
+            get => CashDrawer.Nickels;
+        }
+        
+        public int Dimes
+        {
+            get => CashDrawer.Dimes; 
+        }
+
+
+        public int Quarters
+        {
+            get => CashDrawer.Quarters;
+        }
+
+        public int DollarCents
+        {
+            get => CashDrawer.Dollars;
+        }
+
+        public int Ones
+        {
+            get => CashDrawer.Ones;
+        }
+
+        public int TwoDollars
+        {
+            get => CashDrawer.Twos;
+        }
     }
 }
